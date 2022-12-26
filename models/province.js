f3c18a6bab5e8bb4ee42470b12aa5220b9f1a765
Bobby Rahdyan <bobby.notokoesoemo@gmail.com>
2022-12-26 20:54:26 +0700

    add province model & references

---
+++ models/province.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Province extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Province.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Name is required",
+        },
+        notEmpty: {
+          msg: "Name is required",
+        },
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Province',
+  });
+  return Province;
+};
