b1d2c5fa6ff757c5d02aa30be898a859a96e08e7
Bobby Rahdyan <bobby.notokoesoemo@gmail.com>
2022-12-26 21:47:25 +0700

    add image model

---
+++ models/image.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Image.init({
+    destinationId: DataTypes.INTEGER,
+    imgUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Image Url is required" },
+        notEmpty: { msg: "Image Url is required" },
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Image',
+  });
+  return Image;
+};
