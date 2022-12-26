ec331d084a7e84d3ac210db4b77210334f954178
Bobby Rahdyan <bobby.notokoesoemo@gmail.com>
2022-12-27 06:04:35 +0700

    add data favourite & all seeders

---
+++ seeders/20221226222748-seeder-users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { hashPassword } = require('../helpers/bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const data = require("../data/users.json").map((el) => {
+      el.password = hashPassword(el.password);
+      return {
+        ...el,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    });
+
+    await queryInterface.bulkInsert("Users", data, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Users", null, {});
+  }
+};
---
+++ seeders/20221226222828-seeder-cities.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const data = require("../data/cities.json").map((el) => {
+      delete el.id;
+      return {
+        ...el,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    });
+
+    await queryInterface.bulkInsert("Cities", data, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Cities", null, {});
+  }
+};
