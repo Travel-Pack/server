cb62bfb349d196a0cddd57fd5b4fdbf95fb9fae1
ezar31 <akungugelezar@gmail.com>
2022-12-28 00:25:19 +0700

    Update Destination
    - adding put (update destination & images)
    - adding delete (deleting destination by "id")

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -5,8 +5,9 @@ class DestinationController {
     const t = await sequelize.transaction()
     try {
       const { name, address, mainImg, cost, geocoding, CityId, UserId, imgUrl } = req.body
+      // const UserId = req.user.id
       const newDestination = await Destination.create({ name, address, mainImg, cost, geocoding, CityId, UserId }, { transaction: t }) // , UserId
-      console.log(newDestination, "<<--- newDestination di controller");
+      // console.log(newDestination, "<<--- newDestination di controller");
       if (imgUrl) {
         const images = imgUrl.map(el => {
           return {
@@ -27,15 +28,58 @@ class DestinationController {
   }
   static async readAllDestination(req, res, next) {
     try {
-      const { name, address, mainImg, cost, geocoding, CityId } = req.body
       const destination = await Destination.findAll({
         include: [
-          { model: Review },
-          Image
+          { model: Review, include: [User] },
+          { model: Image, order: [['id', 'DESC']] }
         ]
       })
 
       res.status(200).json(destination)
+    } catch (error) {
+      console.log(error);
+      // next(error)
+    }
+  }
+  static async updateDestination(req, res, next) {
+    const t = await sequelize.transaction()
+    try {
+      const { name, address, mainImg, cost, geocoding, CityId, UserId, imgUrl } = req.body
+      // const UserId = req.user.id
+      const isDestination = await Destination.findByPk(req.params.id)
+      if (isDestination) {
+        const updatedDestination = await Destination.update({ name, address, mainImg, cost, geocoding, CityId, UserId, imgUrl }, { where: { id: req.params.id }, transaction: t })
+
+        const currentImages = await Image.findAll({ where: { DestinationId: isDestination.id, order: [['id', 'ASC']] } })
+        const images = imgUrl.map(el => {
+          return {
+            DestinationId: isDestination.id,
+            imgUrl: el
+          }
+        })
+        if (imgUrl.length == currentImages.length) {// bulkUpdate
+          for (let i = 0; i < imgUrl.length; i++) {
+            await Image.update(images, { where: { id: isDestination.id }, transaction: t })
+          }
+        } else if (imgUrl.length > currentImages.length) { // create / update
+          await Image.bulkCreate(images, { updateOnDuplicate: ["imgUrl"], transaction: t })
+        }
+      } else throw { name: "Destination Not Found" }
+      await t.commit()
+      res.status(201).json(destination)
+    } catch (error) {
+      await t.rollback()
+      console.log(error);
+      // next(error)
+    }
+  }
+  static async deleteDestination(req, res, next) {
+    try {
+      if (await Destination.findByPk(req.params.id)) {
+        const deletedDestination = await Destination.destroy({ where: { id: req.params.id } })
+      } else throw { name: "Destination Not Found" }
+
+      res.status(201).json("Success deleting destionation with ID: " + req.params.id)
     } catch (error) {
       console.log(error);
       // next(error)
--- routers/destinations.js
+++ routers/destinations.js
@@ -4,5 +4,7 @@ const router = require('express').Router()
 
 router.post('/', DestinationController.createDestination)
 router.get('/', DestinationController.readAllDestination)
+router.put('/:id', DestinationController.updateDestination)
+router.delete('/:id', DestinationController.deleteDestination)
 
 module.exports = router
