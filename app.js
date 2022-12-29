340fa872473dcb80926d2278408394e4dab126aa
Qomar034 <nurqomarudin034@gmail.com>
2022-12-30 00:31:07 +0700

    Add Test for Province and minor adjustment

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const router = require("./routers");
-// const errorHandler = require("./middlewares/errorHandler");
+const errorHandler = require("./middlewares/errorHandler");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(router);
-// app.use(errorHandler);
+app.use(errorHandler);
 
 module.exports = app
